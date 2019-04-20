8ca41bd242d0ce715ca1fec42f62830836a99d39
ranggakusuma <ranggavskusuma@gmail.com>
2019-04-20 22:52:46 +0700

    add unique slug

--- Software Engineer/models/job.model.js
+++ Software Engineer/models/job.model.js
@@ -2,13 +2,39 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const jobSchema = new schema({
-  jobName: String,
-  companyName: String,
-  companyLogo: String,
-  jobLocation: String,
+  jobName: {
+    type: String,
+    required: [true, 'Job Name cannot be empty']
+  },
+  companyName: {
+    type: String,
+    required: [true, 'Company Name cannot be empty']
+  },
+  companyLogo: {
+    type: String,
+  },
+  jobLocation: {
+    type: String,
+    required: [true, 'Location cannot be empty']
+  },
   jobFunction: String,
   JobIndustry: String,
-  slug: String,
+  slug: {
+    type: String,
+    required: [true, 'Slug cannot be empty'],
+    validate: [{
+      validator: (v) => {
+        return Job.findOne({
+          slug: v
+        })
+          .then((result) => {
+            throw new Error('Slug has been registered')
+          }).catch((err) => {
+            throw new Error(err.errors)
+          });
+      }
+    }]
+  },
 });
 
 
